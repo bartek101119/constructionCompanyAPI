1f334a0e7dcf49488eafbc081c1a3dbaa13590f9
Bartosz Michalski <bartek10114@wp.pl>
2021-06-11 15:28:53 +0200

    connection to the database

---
+++ constructionCompanyAPI/Entities/ConstructionCompanyDbContext.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace constructionCompanyAPI.Entities
+{
+    public class ConstructionCompanyDbContext : DbContext
+    {
+        private string connectionString = "Server=(localdb)\\mssqllocaldb;Database=ConstructionCompanyDb;Trusted_Connection=True;";
+        public DbSet<ConstructionCompany> ConstructionCompanies { get; set; }
+        public DbSet<Address> Addresses { get; set; }
+        public DbSet<CompanyOwner> CompanyOwners { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<ConstructionCompany>()
+                .Property(c => c.Name)
+                .IsRequired()
+                .HasMaxLength(25);
+
+            modelBuilder.Entity<CompanyOwner>()
+                .Property(c => c.FullName)
+                .IsRequired();
+
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+    }
+}
---
+++ constructionCompanyAPI/Migrations/20210611130947_add-Database.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace constructionCompanyAPI.Migrations
+{
+    public partial class addDatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Addresses",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Voivodeship = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    City = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Street = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PostalCode = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Addresses", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CompanyOwners",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FullName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ContactEmail = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ContactNumber = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CompanyOwners", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ConstructionCompanies",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(25)", maxLength: 25, nullable: false),
+                    LegalForm = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    NIP = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    REGON = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    KRS = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    StartDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ContactEmail = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ContactNumber = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    AddressId = table.Column<int>(type: "int", nullable: false),
+                    CompanyOwnerId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ConstructionCompanies", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ConstructionCompanies_Addresses_AddressId",
+                        column: x => x.AddressId,
+                        principalTable: "Addresses",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ConstructionCompanies_CompanyOwners_CompanyOwnerId",
+                        column: x => x.CompanyOwnerId,
+                        principalTable: "CompanyOwners",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FullName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Specialization = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ConstructionCompanyId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Employees_ConstructionCompanies_ConstructionCompanyId",
+                        column: x => x.ConstructionCompanyId,
+                        principalTable: "ConstructionCompanies",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ConstructionCompanies_AddressId",
+                table: "ConstructionCompanies",
+                column: "AddressId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ConstructionCompanies_CompanyOwnerId",
+                table: "ConstructionCompanies",
+                column: "CompanyOwnerId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Employees_ConstructionCompanyId",
+                table: "Employees",
+                column: "ConstructionCompanyId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Employees");
+
+            migrationBuilder.DropTable(
+                name: "ConstructionCompanies");
+
+            migrationBuilder.DropTable(
+                name: "Addresses");
+
+            migrationBuilder.DropTable(
+                name: "CompanyOwners");
+        }
+    }
+}
