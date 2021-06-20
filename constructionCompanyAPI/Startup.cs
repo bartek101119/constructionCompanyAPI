f0d6d1caca3b3a044cb0c43c784c8e26e1798761
Bartosz Michalski <bartek10114@wp.pl>
2021-06-21 01:25:40 +0200

    dodanie polityki cors

--- constructionCompanyAPI/Startup.cs
+++ constructionCompanyAPI/Startup.cs
@@ -88,11 +88,22 @@ namespace constructionCompanyAPI
             services.AddScoped<IUserContextService, UserContextService>();
             services.AddHttpContextAccessor();
             services.AddScoped<IValidator<ConstructionCompanyQuery>, ConstructionCompanyQueryValidator>();
+            services.AddCors(option =>
+            {
+                option.AddPolicy("FrontEndClient", builder => 
+                     builder.AllowAnyMethod()
+                            .AllowAnyHeader()
+                            .WithOrigins(Configuration["AllowedOrigins"])
+                            
+                 );
+            }); 
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ConstructionCompanySeeder seeder)
         {
+            app.UseCors("FrontEndClient");
+
             seeder.Seed();
 
             if (env.IsDevelopment())
