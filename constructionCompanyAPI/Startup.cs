ec48de588df2b8593eb139b57465719071ddc554
Bartosz Michalski <bartek10114@wp.pl>
2021-06-21 04:00:46 +0200

    dodanie cache'owania

--- constructionCompanyAPI/Startup.cs
+++ constructionCompanyAPI/Startup.cs
@@ -102,6 +102,7 @@ namespace constructionCompanyAPI
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ConstructionCompanySeeder seeder)
         {
+            app.UseResponseCaching();
             app.UseStaticFiles();
             app.UseCors("FrontEndClient");
 
